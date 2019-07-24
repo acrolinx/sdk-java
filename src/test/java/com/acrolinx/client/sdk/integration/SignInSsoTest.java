e3ebe1f8f68fac12c98168f03f0a005504622500
marco <Marco.Stahl@acrolinx.com>
2019-07-24 22:45:56 +0200

    DEV-21144 Skip only the test testSignInWithSSO that needs skipping

--- src/test/java/com/acrolinx/client/sdk/integration/SignInSsoTest.java
+++ src/test/java/com/acrolinx/client/sdk/integration/SignInSsoTest.java
@@ -18,12 +18,13 @@ public class SignInSsoTest {
     @Before
     public void beforeTest() throws URISyntaxException {
         assumeTrue(ACROLINX_URL != null);
-        assumeTrue(ACROLINX_API_USERNAME != null && ACROLINX_API_SSO_TOKEN != null);
         endpoint = createTestAcrolinxEndpoint();
     }
 
     @Test
     public void testSignInWithSSO() throws SSOException {
+        assumeTrue(ACROLINX_API_USERNAME != null && ACROLINX_API_SSO_TOKEN != null);
+
         SignInSuccess signInSuccess = endpoint.signInWithSSO(ACROLINX_API_SSO_TOKEN, ACROLINX_API_USERNAME);
         assertEquals(ACROLINX_API_USERNAME, signInSuccess.getUser().getUsername());
     }
