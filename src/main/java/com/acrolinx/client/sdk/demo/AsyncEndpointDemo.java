f25b727497b80bace5303020803759a9c4ff2762
abhijeetnarvekar <abhijeet.narvekar@acrolinx.com>
2019-08-13 10:06:50 +0200

    DEV-21273 Adding a demo for Async Endpoint java

---
+++ src/main/java/com/acrolinx/client/sdk/demo/AsyncEndpointDemo.java
@@ -0,0 +1,51 @@
+package com.acrolinx.client.sdk.demo;
+
+import com.acrolinx.client.sdk.AcrolinxEndpoint;
+import com.acrolinx.client.sdk.PlatformInformation;
+
+import java.net.URI;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+// Right place for a demo? A separate project
+public class AsyncEndpointDemo {
+
+    AcrolinxEndpoint endpoint;
+    int allowedThreads;
+    ExecutorService executorService;
+
+    public AsyncEndpointDemo(URI acrolinxURL, String clientSignature, String clientVersion, String clientLocale) {
+        this(acrolinxURL, clientSignature, clientVersion, clientLocale, 1);
+
+    }
+
+    public AsyncEndpointDemo(URI acrolinxURL, String clientSignature, String clientVersion, String clientLocale, int allowedThreads) {
+        this.allowedThreads = allowedThreads;
+        this.endpoint = new AcrolinxEndpoint(acrolinxURL, clientSignature, clientVersion, clientLocale);
+        createExecutor();
+    }
+
+    private void createExecutor() {
+        this.executorService = Executors.newFixedThreadPool(allowedThreads);
+    }
+
+    public Future<PlatformInformation> getPlatformInformation() {
+        GetPlatformInformation getPlatformInformationCallable = new GetPlatformInformation(endpoint);
+        return executorService.submit(getPlatformInformationCallable);
+    }
+
+    private static class GetPlatformInformation implements Callable<PlatformInformation> {
+        AcrolinxEndpoint endpoint;
+
+        public GetPlatformInformation(AcrolinxEndpoint endpoint) {
+            this.endpoint = endpoint;
+        }
+
+        @Override
+        public PlatformInformation call() throws Exception {
+            return endpoint.getPlatformInformation();
+        }
+    }
+}
---
+++ src/test/java/com/acrolinx/client/sdk/integration/AsyncDemoTest.java
@@ -0,0 +1,28 @@
+package com.acrolinx.client.sdk.integration;
+
+import com.acrolinx.client.sdk.PlatformInformation;
+import com.acrolinx.client.sdk.demo.AsyncEndpointDemo;
+import org.junit.Test;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+import static com.acrolinx.client.sdk.integration.common.CommonTestSetup.ACROLINX_URL;
+import static com.acrolinx.client.sdk.testutils.TestConstants.DEVELOPMENT_SIGNATURE;
+import static org.junit.Assert.assertNotNull;
+
+public class AsyncDemoTest {
+
+    @Test
+    public void testFetchingPlatformInformationAsync() throws InterruptedException, ExecutionException, URISyntaxException {
+        AsyncEndpointDemo asyncEndpointDemo = new AsyncEndpointDemo(new URI(ACROLINX_URL), DEVELOPMENT_SIGNATURE,
+                "1.2.3.4", "en");
+
+        Future<PlatformInformation> platformInformationFuture = asyncEndpointDemo.getPlatformInformation();
+        PlatformInformation platformInformation = platformInformationFuture.get();
+
+        assertNotNull(platformInformation);
+    }
+}
