9a2e7b57cf6197dfbeb6a68895a1cd3e5ffafae6
franziska <franziska.preiss@acrolinx.com>
2019-08-21 17:55:05 +0200

    DEV-21456 Added spotless task to run automatically on gradle build task
    * Run spotless

--- src/test/java/com/acrolinx/client/sdk/wiremock/HttpClientMockTest.java
+++ src/test/java/com/acrolinx/client/sdk/wiremock/HttpClientMockTest.java
@@ -1,14 +1,11 @@
 /**
  * Copyright (c) 2019-present Acrolinx GmbH
  */
+
 package com.acrolinx.client.sdk.wiremock;
 
-import com.acrolinx.client.sdk.exceptions.AcrolinxException;
-import com.acrolinx.client.sdk.http.AcrolinxResponse;
-import com.acrolinx.client.sdk.http.ApacheHttpClient;
-import com.acrolinx.client.sdk.http.HttpMethod;
-import com.acrolinx.client.sdk.wiremock.common.MockedTestBase;
-import org.junit.Test;
+import static com.acrolinx.client.sdk.wiremock.common.WireMockUtils.*;
+import static junit.framework.TestCase.assertTrue;
 
 import java.io.IOException;
 import java.net.URI;
@@ -16,41 +13,53 @@ import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.concurrent.*;
 
-import static com.acrolinx.client.sdk.wiremock.common.WireMockUtils.*;
-import static junit.framework.TestCase.assertTrue;
+import org.junit.Test;
 
-public class HttpClientMockTest extends MockedTestBase {
+import com.acrolinx.client.sdk.exceptions.AcrolinxException;
+import com.acrolinx.client.sdk.http.AcrolinxResponse;
+import com.acrolinx.client.sdk.http.ApacheHttpClient;
+import com.acrolinx.client.sdk.http.HttpMethod;
+import com.acrolinx.client.sdk.wiremock.common.MockedTestBase;
+
+public class HttpClientMockTest extends MockedTestBase
+{
 
     public static final int PLATFORM_PORT_MOCKED = 8089;
     public static final String acrolinxUrl = "http://localhost:" + PLATFORM_PORT_MOCKED;
 
     @Test
-    public void test404HttpClient() throws URISyntaxException, IOException, AcrolinxException {
+    public void test404HttpClient() throws URISyntaxException, IOException, AcrolinxException
+    {
         String api = "/api/v1/";
         httpClientMockNotFoundResponse(api);
         HashMap<String, String> headersHashMap = new HashMap<>();
         ApacheHttpClient apacheHttpClient = new ApacheHttpClient();
-        AcrolinxResponse acrolinxResponse = apacheHttpClient.fetch(new URI(acrolinxUrl + api), HttpMethod.GET, headersHashMap, null);
+        AcrolinxResponse acrolinxResponse = apacheHttpClient.fetch(new URI(acrolinxUrl + api), HttpMethod.GET,
+                headersHashMap, null);
 
         assertTrue(acrolinxResponse.getStatus() == 404);
 
     }
 
     @Test
-    public void testTimeout() throws URISyntaxException, IOException, AcrolinxException {
+    public void testTimeout() throws URISyntaxException, IOException, AcrolinxException
+    {
 
         String api = "/api/v1/";
         httpClientMockTimeOut(api);
         HashMap<String, String> headersHashMap = new HashMap<>();
         ApacheHttpClient apacheHttpClient = new ApacheHttpClient();
-        AcrolinxResponse acrolinxResponse = apacheHttpClient.fetch(new URI(acrolinxUrl + api), HttpMethod.GET, headersHashMap, null);
+        AcrolinxResponse acrolinxResponse = apacheHttpClient.fetch(new URI(acrolinxUrl + api), HttpMethod.GET,
+                headersHashMap, null);
 
         assertTrue(acrolinxResponse.getStatus() == 408);
 
     }
 
     @Test
-    public void testTimeoutParallelConections() throws URISyntaxException, IOException, ExecutionException, InterruptedException, AcrolinxException {
+    public void testTimeoutParallelConections()
+            throws URISyntaxException, IOException, ExecutionException, InterruptedException, AcrolinxException
+    {
 
         final String api = "/api/v1/";
         httpClientMockTimeOut(api);
@@ -60,13 +69,15 @@ public class HttpClientMockTest extends MockedTestBase {
         Future<AcrolinxResponse> acrolinxResponseFuture = executorService.submit(new Callable<AcrolinxResponse>() {
 
             @Override
-            public AcrolinxResponse call() throws Exception {
+            public AcrolinxResponse call() throws Exception
+            {
                 return apacheHttpClient.fetch(new URI(acrolinxUrl + api), HttpMethod.GET, headersHashMap, null);
             }
         });
 
         httpClientMockSuccess(api);
-        AcrolinxResponse ar200 = apacheHttpClient.fetch(new URI(acrolinxUrl + api), HttpMethod.GET, headersHashMap, null);
+        AcrolinxResponse ar200 = apacheHttpClient.fetch(new URI(acrolinxUrl + api), HttpMethod.GET, headersHashMap,
+                null);
         assertTrue(ar200.getStatus() == 200);
         assertTrue(acrolinxResponseFuture.get().getStatus() == 408);
 
