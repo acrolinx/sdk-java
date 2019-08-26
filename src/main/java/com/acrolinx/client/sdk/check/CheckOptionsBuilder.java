e2b8148f815b6da1d987009f36a1fb7388ac0558
abhijeetnarvekar <abhijeet.narvekar@acrolinx.com>
2019-08-26 17:31:10 +0200

    Added javadoc for check type

--- src/main/java/com/acrolinx/client/sdk/check/CheckOptionsBuilder.java
+++ src/main/java/com/acrolinx/client/sdk/check/CheckOptionsBuilder.java
@@ -39,6 +39,31 @@ public class CheckOptionsBuilder
         return this;
     }
 
+    /**
+     * <p>How to choose a correct Check Type?
+     * Following are different use cases of the check with corresponding check type:
+     * <table summary="" border="1">
+     *  <tr>
+     *    <td> Check Type </td> <td> Ux </td><td> Use case </td>
+     *  </tr>
+     *  <tr>
+     *    <td> interactive </td> <td> Interactive </td><td> User checks her own document. </td>
+     *  </tr>
+     *  <tr>
+     *    <td> batch </td> <td> Interactive </td><td> User checks set of her own documents. </td>
+     *  </tr>
+     *  <tr>
+     *    <td> baseline </td> <td> Noninteractive </td><td> A repository of documents are checked as part of build job running nightly and the user doesn't own the documents. </td>
+     *  </tr>
+     *  <tr>
+     *    <td> automated  </td> <td> Noninteractive  </td><td> Check of a single document for automated scenarios as for example a git hook or save a document. </td>
+     *  </tr>
+     * </table>
+     *
+     *
+     * @param checkType  Based on the origin of check
+     * @return CheckOptionsBuilder
+     */
     public CheckOptionsBuilder withCheckType(CheckType checkType)
     {
         this.checkType = checkType;
