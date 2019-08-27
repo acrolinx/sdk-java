919a96f7efe2b3fd56b3b9c228bf27fd77b6ccd9
marco <Marco.Stahl@acrolinx.com>
2019-08-27 12:04:23 +0200

    Fix Sonar warnings

--- src/main/java/com/acrolinx/client/sdk/check/CheckRequestBuilder.java
+++ src/main/java/com/acrolinx/client/sdk/check/CheckRequestBuilder.java
@@ -4,12 +4,10 @@
 
 package com.acrolinx.client.sdk.check;
 
+import com.acrolinx.client.sdk.exceptions.AcrolinxException;
+
 import java.util.ArrayList;
 import java.util.List;
-
-import javax.annotation.Nullable;
-
-import com.acrolinx.client.sdk.exceptions.AcrolinxException;
 
 public class CheckRequestBuilder
 {
