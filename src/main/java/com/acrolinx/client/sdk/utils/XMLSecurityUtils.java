e8c3dc8bc0ddcb482be873acc59f847900260791
Abhijeet Narvekar <abhijeet.narvekar@acrolinx.com>
2021-08-03 14:32:23 +0200

    DEV-24850 SONAR - limit resolution of external entities

---
+++ src/main/java/com/acrolinx/client/sdk/utils/XMLSecurityUtils.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2021-present Acrolinx GmbH
+ */
+
+package com.acrolinx.client.sdk.utils;
+
+import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.TransformerFactory;
+
+public class XMLSecurityUtils
+{
+
+    public static void limitResolutionOfExternalEntities(DocumentBuilderFactory factory)
+    {
+        // prohibit the use of all protocols by external entities:
+        factory.setAttribute(XMLConstants.ACCESS_EXTERNAL_DTD, "");
+        factory.setAttribute(XMLConstants.ACCESS_EXTERNAL_SCHEMA, "");
+    }
+
+    public static void limitResolutionOfExternalEntities(TransformerFactory factory)
+    {
+        // to be compliant, prohibit the use of all protocols by external entities:
+        factory.setAttribute(XMLConstants.ACCESS_EXTERNAL_DTD, "");
+        factory.setAttribute(XMLConstants.ACCESS_EXTERNAL_STYLESHEET, "");
+    }
+
+}
