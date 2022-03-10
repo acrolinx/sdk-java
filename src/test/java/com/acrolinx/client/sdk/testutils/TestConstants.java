9279b07d8e6313cac0d1ff071e58806c46c5ac87
Abhijeet Narvekar <abhijeet.narvekar@acrolinx.com>
2022-03-10 11:46:13 +0100

    DEV-29301 Use new dev signature

--- src/test/java/com/acrolinx/client/sdk/testutils/TestConstants.java
+++ src/test/java/com/acrolinx/client/sdk/testutils/TestConstants.java
@@ -4,7 +4,13 @@
 
 package com.acrolinx.client.sdk.testutils;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class TestConstants
 {
-    public static final String DEVELOPMENT_SIGNATURE = "SW50ZWdyYXRpb25EZXZlbG9wbWVudERlbW9Pbmx5";
+    static Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+    public static final String ACROLINX_DEV_SIGNATURE = dotenv.get("ACROLINX_DEV_SIGNATURE");
+    public static final String DEVELOPMENT_SIGNATURE = ACROLINX_DEV_SIGNATURE.isEmpty()
+            ? "SW50ZWdyYXRpb25EZXZlbG9wbWVudERlbW9Pbmx5"
+            : ACROLINX_DEV_SIGNATURE;
 }
