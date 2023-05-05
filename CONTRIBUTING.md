8da9237ffe552d138fcfa2742376dd26694999cd
Thilo Mühlberg <thilo.muehlberg@acrolinx.com>
2023-05-05 08:51:01 +0200

    Update spotless gradle plugin, align config with other projects
    Also remove vscode settings file.

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -5,23 +5,23 @@
 1. Check out the project, create, and configure an `.env` file as described above.
 
 2. Please add new features using the `master` branch, or submit a pull request.
-   To install dependencies, build, and test the project, run:
+To install dependencies, build, and test the project, run:
 
-   ```bash
-    ./gradlew build
-   ```
+```bash
+	./gradlew build
+```
 
-   Also run the same command before pushing your changes to GitHub.
+Also run the same command before pushing your changes to GitHub.
 
-   On Travis the build will be tested against the latest released Acrolinx Platform.
-   If your build on Travis was successful, a new snapshot version will be automatically available via the [Maven snapshot repository](https://oss.sonatype.org/content/repositories/snapshots/com/acrolinx/client/sdk/).
+On Travis the build will be tested against the latest released Acrolinx Platform.
+If your build on Travis was successful, a new snapshot version will be automatically available via the [Maven snapshot repository](https://oss.sonatype.org/content/repositories/snapshots/com/acrolinx/client/sdk/).
 3. Once you tested your new features, remove the `-SNAPSHOT` from the `CURRENT_VERSION` property in the [`gradle.properties`](gradle.properties) file.
 4. Commit and push your changes. If all goes right, the artifact is released to [Maven Central](https://search.maven.org/#search%7Cga%7C1%7Cg%3A%22com.acrolinx.client%22%20a%3A%22sdk%22%20).
 Note that it might take quite a while until the new version shows up in [Maven Central](https://search.maven.org/#search%7Cga%7C1%7Cg%3A%22com.acrolinx.client%22%20a%3A%22sdk%22%20).
 
-   If that build fails, you might have to sign in into [Nexus Repository Manager](https://oss.sonatype.org/#welcome) and drop falsely created repositories, before starting a new release build.
+If that build fails, you might have to sign in into [Nexus Repository Manager](https://oss.sonatype.org/#welcome) and drop falsely created repositories, before starting a new release build.
 
-   TravisCI will also automatically create a release tag on GitHub.
+TravisCI will also automatically create a release tag on GitHub.
 
 5. Once the tag is pushed to GitHub, Actions will automatically update the [API documentation on the GitHub Pages](https://acrolinx.github.io/sdk-java/).
 6. Don't forget to commit and push a new SNAPSHOT version.
--- deploy.sh
+++ deploy.sh
@@ -5,17 +5,17 @@ echo "Starting publish script"
 GRADLE_PROPERTIES_FILE=gradle.properties
 
 is_not_substring(){
-     if [ -z "${2##*$1*}" ]; then
-            return 1
-        else return 0
-     fi
+	if [ -z "${2##*$1*}" ]; then
+			return 1
+		else return 0
+	fi
 }
 
 getProperty()
 {
-    PROP_KEY=$1
-    PROP_VALUE=`cat "./gradle.properties" | grep "$PROP_KEY" | cut -d'=' -f2`
-    echo $PROP_VALUE
+	PROP_KEY=$1
+	PROP_VALUE=`cat "./gradle.properties" | grep "$PROP_KEY" | cut -d'=' -f2`
+	echo $PROP_VALUE
 }
 
 
@@ -23,33 +23,33 @@ PROJECT_VERSION=$(getProperty "CURRENT_VERSION")
 echo "Current Version: $PROJECT_VERSION"
 
 if [[ "$PROJECT_VERSION" == *"SNAPSHOT"* ]]; then
-    echo "Publishing snapshot version to snapshot repo..."
-    if ./gradlew publishToSonatype; then
-        echo "Published snapshot version to snapshot repo..."
-        exit 0
-    else
-        exit 1
-    fi
+	echo "Publishing snapshot version to snapshot repo..."
+	if ./gradlew publishToSonatype; then
+		echo "Published snapshot version to snapshot repo..."
+		exit 0
+	else
+		exit 1
+	fi
 else
-    echo "Publishing release version to staging repo..."
-    if ./gradlew publishToSonatype; then
-        echo "Done with publish step."
-        echo "Starting close and release step"
-        if ./gradlew closeAndReleaseRepository; then
-            echo "Done with release step."
-            echo "Trying to create Github Release Tag"
-            if ./gradlew createGithubReleaseTag; then
-              echo "::set-output name=TAGNAME::release-$PROJECT_VERSION"
-              echo "::set-output name=RELEASE::true"
-              exit 0
-            else
-              echo "Can't create Github Release Tag. Please do manually."
-              exit 1
-            fi
-        else
-            exit 1
-        fi
-    else
-        exit 1
-    fi
+	echo "Publishing release version to staging repo..."
+	if ./gradlew publishToSonatype; then
+		echo "Done with publish step."
+		echo "Starting close and release step"
+		if ./gradlew closeAndReleaseRepository; then
+			echo "Done with release step."
+			echo "Trying to create Github Release Tag"
+			if ./gradlew createGithubReleaseTag; then
+			echo "::set-output name=TAGNAME::release-$PROJECT_VERSION"
+			echo "::set-output name=RELEASE::true"
+			exit 0
+			else
+			echo "Can't create Github Release Tag. Please do manually."
+			exit 1
+			fi
+		else
+			exit 1
+		fi
+	else
+		exit 1
+	fi
 fi
